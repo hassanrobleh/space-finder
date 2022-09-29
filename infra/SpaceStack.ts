59da997388760421df7d2ce5ce8fb92301c32471
hassanrobleh <hassan.alirobleh@gmail.com>
2022-09-29 13:31:40 +0200

    cdk node lambda

--- infra/SpaceStack.ts
+++ infra/SpaceStack.ts
@@ -1,12 +1,16 @@
-import { Stack, StackProps } from "aws-cdk-lib"
+import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { Code, Function as LambdaFunction, Runtime } from "aws-cdk-lib/aws-lambda"
-import { join } from "path"
-import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway"
+import { Code, Function as LambdaFunction, Runtime } from "aws-cdk-lib/aws-lambda";
+import { join } from "path";
+import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
+import { GenericTable } from '../GenericTable';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
 
 export class SpaceStack extends Stack {
 
-    private api = new RestApi(this, 'SpaceApi')
+    private api = new RestApi(this, 'SpaceApi');
+
+    //private spaceTable = new GenericTable('spaceTable', 'spaceId', this);
 
     constructor(scope: Construct, id:string, props:StackProps) {
         super(scope, id, props)
@@ -15,7 +19,14 @@ export class SpaceStack extends Stack {
             runtime: Runtime.NODEJS_14_X,
             code: Code.fromAsset(join(__dirname, '..', 'service', 'hello')),
             handler: 'hello.main'
-        })
+        });
+
+        new GenericTable('spaceTable', 'spaceId', this)
+
+        new NodejsFunction(this, 'hLambdaNodejs', {
+            entry: (join(__dirname, '..', 'services', 'node-lambda', 'hello.ts')),
+            handler: 'handler'
+        });
 
         // Hello Api lambda integration
         const helloLambdaIntegration = new LambdaIntegration(helloLambda)
